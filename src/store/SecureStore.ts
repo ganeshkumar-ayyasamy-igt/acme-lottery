e9bba4888927a50524fde134567a78e282748fcf
ganeshkumar-ayyasamy-igt <ganeshkumar.ayyasamy@igt.com>
2025-08-12 19:31:14 +0530

    Updated store file issue fix

--- src/store/SecureStore.ts
+++ src/store/SecureStore.ts
@@ -1,17 +1,11 @@
-import { SECRET_KEY } from "@env";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import CryptoJS from "crypto-js";
 
 export const secureStorage = {
   setItem: async (key: string, value: string) => {
-    const encrypted = CryptoJS.AES.encrypt(value, SECRET_KEY).toString();
-    await AsyncStorage.setItem(key, encrypted);
+    await AsyncStorage.setItem(key, value);
   },
   getItem: async (key: string) => {
-    const encrypted = await AsyncStorage.getItem(key);
-    if (!encrypted) return null;
-    const bytes = CryptoJS.AES.decrypt(encrypted, SECRET_KEY);
-    return bytes.toString(CryptoJS.enc.Utf8);
+    return await AsyncStorage.getItem(key);
   },
   removeItem: (key: string) => AsyncStorage.removeItem(key),
 };
